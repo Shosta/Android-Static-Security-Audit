ef705f2d8c9c265366ae86bd6d03612fe191e11b
Unknown <remi.lavedrine@outlook.com>
2018-07-12 17:04:28 +0200

    Display verbose logging on the repackaging steps

--- PythonScript/repackageapp.py
+++ PythonScript/repackageapp.py
@@ -4,6 +4,7 @@
 '''
 import bcolors
 import cmdutils
+import logging
 
 def unzip_package(package_name):
     '''
@@ -18,7 +19,9 @@ def unzip_package(package_name):
     output = cmdutils.launchcmdreturnoutput(cmd)
     #process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None, shell=True)
     #output = process.communicate()
-    print(output[0]) # Verbose
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
 
 def disassemble_package(package_name):
     """
@@ -32,44 +35,65 @@ def disassemble_package(package_name):
     cmd = "apktool d " + "/tmp/Attacks/SourcePackage/" +  package_name + ".apk" + " -f -o " + "/tmp/Attacks/DecodedPackage"
 
     output = cmdutils.launchcmdreturnoutput(cmd)
-    print(output)
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
 
 def make_application_debuggable():
+    '''
+    Use sed command to add debuggable=true to the Application Manifest.
+    '''
     #Use sed method to add debuggable=true to the application manifest.
     print(bcolors.OKGREEN + "Make app debuggable" + bcolors.ENDC +  " using " + bcolors.BOLD + "sed" + bcolors.ENDC + " command")
     cmd = "sed -i -e 's/<application /<application android:debuggable=\"true\" /' /tmp/Attacks/DecodedPackage/AndroidManifest.xml"
 
     output = cmdutils.launchcmdreturnoutput(cmd)
-    print(output)    
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)  
 
 def allow_backup():
-    #Use grep and sed to allow backup on the application.
+    '''
+    Use grep and sed commands to allow backup on the application through the Application Manifest.
+    '''
     print(bcolors.OKGREEN + "Allow backup on app" + bcolors.ENDC + " using " + bcolors.BOLD + "sed" + bcolors.ENDC + " command")
     cmd = "sed -i -e 's/android:allowBackup=\"false\" / /' /tmp/Attacks/DecodedPackage/AndroidManifest.xml"
     cmdutils.launchcmd(cmd)
 
     cmd = "sed -i -e 's/<application /<application android:allowBackup=\"true\" /' /tmp/Attacks/DecodedPackage/AndroidManifest.xml"
     output = cmdutils.launchcmdreturnoutput(cmd)
-    print(output[0])
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
 
 def repackage_debuggable_application(package_name):
+    '''
+    
+    '''
     print(bcolors.OKGREEN + "Repackage the app" + bcolors.ENDC + " using " + bcolors.BOLD + "apktool" + bcolors.ENDC)
     cmd = "apktool b /tmp/Attacks/DecodedPackage -o /tmp/Attacks/DebuggablePackage/" + package_name + ".b.apk"
     output = cmdutils.launchcmdreturnoutput(cmd)
-    print(output[0])
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
 
 def sign_apk(package_name):
     print(bcolors.OKGREEN + "Sign the application " + bcolors.ENDC + "using " + bcolors.BOLD + "appium/signapk" + bcolors.ENDC + " tool")
     cmd = "signapk /tmp/Attacks/DebuggablePackage/" + package_name + ".b.apk"
     output = cmdutils.launchcmdaliasreturnoutput(cmd)
-    print(output[0])
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
 
 def reinstall_app(package_name):
     print(bcolors.OKGREEN + "Uninstall the production app from the device" + bcolors.ENDC)
     cmd = "adb uninstall " + package_name
     cmdutils.launchcmd(cmd)
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
 
     print(bcolors.OKGREEN + "Reinstall the debuggable app to the device" + bcolors.ENDC)
     cmd = "adb install /tmp/Attacks/DebuggablePackage/" + package_name + ".b.s.apk"
     output = cmdutils.launchcmdreturnoutput(cmd)
-    print(output[0])
+    
+    logging.display_verbose_logging(output[0])
+    logging.display_logging(bcolors.BOLD + "Done" + bcolors.ENDC)
